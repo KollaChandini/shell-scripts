1ea8351ceeb97fd6ab6b9f1159c655db2365d150
Kolla Chandini <119381446+KollaChandini@users.noreply.github.com>
2024-06-15 13:59:19 +0530

    Update aws-cli-install.sh

--- aws-cli-install.sh
+++ aws-cli-install.sh
@@ -3,3 +3,5 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo apt install unzip
 unzip awscliv2.zip
 sudo ./aws/install
+#after installation execute "aws configure" to set up your AWS credentials and configuration.
+#By this we can interact with the aws services from command line more secure
