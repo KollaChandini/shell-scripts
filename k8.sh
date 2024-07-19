721eb4530da5fe62f352919fc0d99fb73236adca
Kolla Chandini <119381446+KollaChandini@users.noreply.github.com>
2024-07-19 12:24:48 +0530

    Create k8.sh

---
+++ k8.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#ON MASTER AND WORKER NODES
+sudo apt-get update
+sudo apt-get install docker.io -y
+sudo apt-get install -y apt-transport-https ca-certificates curl gpg
+sudo mkdir -p -m 755 /etc/apt/keyrings
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+
+
+
+#(ON MASTER NODE)
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 
+
+#after executing the above cmd it will generate token. copy that generated token and paste it in the worker nodes to join them in the cluster
+
+#(ON MASTER NODE)
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.49.0/deploy/static/provider/baremetal/deploy.yaml
